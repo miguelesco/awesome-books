b7203d8b15dfb71027817e96a051d1bde7eaeda0
miguelesco <miuelangel1999@gmail.com>
2021-07-12 20:37:28 -0400

    remove problem solve

--- modules/book_handler.js
+++ modules/book_handler.js
@@ -5,7 +5,9 @@ const form = document.querySelector('.form');
 const BookHandler = {
   init: () => {
     form.addEventListener('submit', (event) => BookHandler.add(event));
-
+    BookHandler.checkBtns();
+  },
+  checkBtns: () => {
     const button = Array.from(Books.bookList.getElementsByClassName('removeBtn'));
     button.forEach((btn, i) => {
       btn.addEventListener('click', () => BookHandler.remove(i));
@@ -21,12 +23,14 @@ const BookHandler = {
     storageBooks.push({ title, author });
     localStorage.setItem('books', JSON.stringify(storageBooks));
     Books.render();
+    BookHandler.checkBtns();
   },
   remove: (index) => {
     const localStorageItems = JSON.parse(localStorage.getItem('books'));
     const newItems = localStorageItems.filter((item, i) => i !== index);
     localStorage.setItem('books', JSON.stringify(newItems));
     Books.render();
+    BookHandler.checkBtns();
   },
 };
 
